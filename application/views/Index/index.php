5629d0a5b5fe5d31feaff6039e9e367fc08bbcdf
MH <icefire19966@yahoo.com>
2016-10-22 14:57:53 +0800

    hello git

---
+++ application/views/Index/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Taichung Tour</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+
+	<link rel="stylesheet" type="text/css" href=<?=base_url('assets/css/Main.css')?>>
+	
+</head>
+
+<style type="text/css">	
+
+</style>
+
+<body>
+	<header>
+		<div id="banner" class="col-md-12"></div>
+		<div id="top">
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators col-md-12">
+					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				</ol>
+
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner" role="listbox">
+					<div class="item active">
+						<img src="<?=base_url('assets/img/016.jpg')?>">
+						<div class="carousel-caption">
+							
+						</div>
+					</div>
+					<div class="item">
+						<img src=<?=base_url('assets/img/img1.jpg')?>>
+						<div class="carousel-caption">
+							
+						</div>
+					</div>
+
+					<div class="item">
+						<img src=<?=base_url('assets/img/020.jpg')?>>
+						<div class="carousel-caption">
+							
+						</div>
+					</div>
+					
+				</div>
+
+				<!-- Controls -->
+				<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+		</div>
+	</header>
+	<div id="container2" class="col-md-12"></div>
+	<div id="container3" class="col-md-12"></div>
+	<div id="container4" class="col-md-12"></div>
+	<div id="container5" class="col-md-12"></div>
+	<footer class="col-md-12">
+		<div id="under" ></div>
+		<div id="down"></div>
+	</footer>
+</body>
+</html>
