f8d5000e4fd66a408c9b115a33c42283e85c3c64
Finnick lee <da7981614@gmail.com>
2016-10-22 16:29:49 +0800

    newLocation Back

--- application/migrations/001_basic_db.php
+++ application/migrations/001_basic_db.php
@@ -48,6 +48,45 @@ class Migration_Basic_db extends CI_Migration {
         $this->dbforge->add_key('id', TRUE);
         $this->dbforge->create_table('test');
         /*the end of products table*/
+
+         /**
+         * location table
+         *  
+         * id: primary key, serial number (int)
+         *
+         * img_path: (text)
+         *
+         * description: (text)
+         */
+        $this->dbforge->add_field(array(
+            'lc_id' => array(
+                'type' => 'INT',
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'lc_name' => array(
+                'type' => 'TEXT',
+            ),
+            'img_path' => array(
+                'type' => 'TEXT',
+            ),
+            'description' => array(
+                'type' => 'TEXT',
+            ),
+            'address' => array(
+                'type' => 'TEXT',
+            ),
+            'lat' => array(
+                'type' => 'VARCHAR(50)',
+            ),
+            'lng' => array(
+                'type' => 'VARCHAR(50)',
+            ),
+            
+        ));
+        $this->dbforge->add_key('lc_id', TRUE);
+        $this->dbforge->create_table('location');
+        /*the end of location table*/
     }
 
     public function down()
