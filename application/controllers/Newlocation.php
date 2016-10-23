088ef7ce966f46d2d714004ed3fa12d104b67f69
Jim <spf0122jim@gmail.com>
2016-10-23 12:03:30 +0800

    Fix

--- application/controllers/Newlocation.php
+++ application/controllers/Newlocation.php
@@ -11,55 +11,45 @@ class Newlocation extends CI_Controller {
         
         public function index()
         {
-        $data = $this->Newlocation_model->get_location();
-        $title = '景點管理';
-        $this->load->view('back/header');
-        $this->load->view('newlocation/index', array('data' => $data));
-        $this->load->view('back/footer');
+          $data = $this->Newlocation_model->get_location();
+          $title = '景點管理';
+          $this->load->view('back/header');
+          $this->load->view('newlocation/index', array('data' => $data));
+          $this->load->view('back/footer');
         }
 
-        public function view($data = NULL)
-        {
-        $data['news_item'] = $this->Newlocation_model->get_location($slug);
-
-        if (empty($data['news_item']))
-        {
-                show_404();
-        }
-
-        $data['title'] = $data['news_item']['title'];
-
-        $this->load->view('newlocation/header');
-        $this->load->view('newlocation/view', $data);
-        $this->load->view('newlocation/footer');
-        }
-        
         public function create()
         {
-        $this->load->helper('form');
-        $this->load->library('form_validation');
+          $this->load->helper('form');
+          $this->load->library('form_validation');
 
-        $data['title'] = '新增景點';
-     
-        $this->form_validation->set_rules('lc_name', 'lc_name', 'required');
-        $this->form_validation->set_rules('img_path', 'img_path', 'required');
-        $this->form_validation->set_rules('description', 'description', 'required');
-        $this->form_validation->set_rules('address', 'address', 'required');
-        $this->form_validation->set_rules('lat', 'lat', 'required');
-        $this->form_validation->set_rules('lng', 'lng', 'required');
+          $data['title'] = '新增景點';
+      
+          $this->form_validation->set_rules('lc_name', 'lc_name', 'required');
+          $this->form_validation->set_rules('img_path', 'img_path', 'required');
+          $this->form_validation->set_rules('description', 'description', 'required');
+          $this->form_validation->set_rules('address', 'address', 'required');
+          $this->form_validation->set_rules('lat', 'lat', 'required');
+          $this->form_validation->set_rules('lng', 'lng', 'required');
 
-        if ($this->form_validation->run() === FALSE)
-        {
-            $this->load->view('back/header.php', $data);
-            $this->load->view('newlocation/create.php');
-            $this->load->view('back/footer.php');
+          if ($this->form_validation->run() === FALSE)
+          {
+              $this->load->view('back/header.php', $data);
+              $this->load->view('newlocation/create.php');
+              $this->load->view('back/footer.php');
 
+          }
+          else
+          {
+              $this->Newlocation_model->set_news();
+              $this->load->view('newlocation/success');
+              redirect(site_url("newlocation/index"));
+          }
         }
-        else
-        {
-            $this->Newlocation_model->set_news();
-            $this->load->view('newlocation/success');
-            redirect(site_url("newlocation/index"));
-        }
+        //Api        
+        public function query(){          
+          $q = $this->input->get('q');          
+          $data = $this->Newlocation_model->get_location()->result_array();          
+          echo json_encode($data, JSON_UNESCAPED_UNICODE);        
         }
 }
