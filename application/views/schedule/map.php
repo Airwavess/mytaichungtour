22889c4f926ffa0e00fa3a34a86bd9e0404634b5
Jim <spf0122jim@gmail.com>
2016-11-10 18:53:23 +0800

    洗個澡

--- application/views/schedule/map.php
+++ application/views/schedule/map.php
@@ -1,51 +1,66 @@
-<!DOCTYPE html>
-<html>
+    <style>
+      /* This is stylesheet */
+      /* Jim 2016/11/10 */
+      .fun-bar {
+        background-color: #505050;
+        z-index:2;position:fixed;
+        top:50px;left:0;
+        width:100%;
+      }
 
-<head>
+      .fun-bar a:hover{
+        color: #000000!important;
+      }
 
-    <title>台中行程規劃小幫手</title>
+      .fun-bar .active a:hover{
+        color: #FFFFFF!important;
+      }
 
-    <meta name="viewport" content="initial-scale=1.0">
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
+      .fun-bar li a{
+        border-radius: 0%!important;
+      }
 
-<body style="margin:0;padding:0;height:100vh;overflow-y: hidden;overflow-x: hidden;font-family: Microsoft JhengHei;">
-    <nav class="navbar navbar-default" style="top:0px;z-index:2;background-color: #96e7d6;">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#" style="font-weight: 900;font-size:25px;">台中行程規劃小幫手</a>
-                <ul class="nav navbar-nav">
-                    <li><a href="#" onclick="initMap()">清除路線</a></li>
+      .mapview {
+        height:100vh;
+        top: 0px; 
+        z-index:1;
+      }
+
+      .msg {
+        z-index:100;
+        position:absolute;
+        top: 50px;
+      }
+
+    </style>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 fun-bar">
+              <div class="container">
+                <ul class="nav nav-pills" id="fun-bar">
+                    <li><a href="#add">新增地點</a></li>
+                    <li><a href="#route">路線規劃</a></li>
+                    <li><a href="#map">查看地圖</a></li>
                 </ul>
+               </div> 
             </div>
         </div>
-    </nav>
-    <div id="msg" class="col-md-12" style="z-index:100;position:absolute;top: 50px;"></div>
-    <div class="row">
-        <div class="list-group col-md-3" style="position:absolute;z-index:2;top:60px;left:10px;height:410px;padding-right: 0;">
-            <a href="#" class="list-group-item active">選擇地點</a>
-            <div id="line" style="overflow-y: auto;height:100%;background-color:#fff;"></div>
-            <button type="button" id="more" class="list-group-item" onclick="" style="font-size:20px;background-color:#CCC;">下一頁</button>
-        </div>
-        <div id="selectedList" class="list-group col-md-3" style="position:fixed;z-index:2;top:60px;left:370px;height:410px;;padding-right: 0;">
-            <a href="#" class="list-group-item active">已選擇地點</a>
-            <div id="selected" style="overflow-y: auto;height:100%;background-color:#fff;"></div>
-            <button type="button" class="list-group-item" onclick="calculateAndDisplayRoute()" style="font-size:20px;background-color:#CCC;">計算路線</button>
-        </div>
     </div>
-    <div id="mapview" style="height:100%;top: -80px;z-index:1;"></div>
-</body>
-<script src="<?=base_url("assets/scripts/map.js")?>"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBeomCbkr7arq7I2GQlw9hrE-zdso8SoVQ&libraries=places&callback=initMap" async defer></script>
-
-</html>
+    <div id="msg" class="col-md-12"></div>
+    <div class="container">
+      <div class="row">
+          <!--<div class="list-group col-md-3" style="position:absolute;z-index:2;top:60px;left:10px;height:410px;padding-right: 0;">
+              <a href="#" class="list-group-item active">選擇地點</a>
+              <div id="line" style="overflow-y: auto;height:100%;background-color:#fff;"></div>
+              <button type="button" id="more" class="list-group-item" onclick="" style="font-size:20px;background-color:#CCC;">下一頁</button>
+          </div>-->
+          <!--<div id="selectedList" class="list-group col-md-3" style="position:fixed;z-index:1;top:60px;left:370px;height:410px;;padding-right: 0;">
+              <a href="#" class="list-group-item active">已選擇地點</a>
+              <div id="selected" style="overflow-y: auto;height:100%;background-color:#fff;"></div>
+              <button type="button" class="list-group-item" onclick="calculateAndDisplayRoute()" style="font-size:20px;background-color:#CCC;">計算路線</button>
+          </div>-->
+          <div class="col-md-12 mapview" id="View">
+          </div>
+       </div>   
+    </div>
+    
--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -10,12 +10,12 @@ var selectArray = [];
 var waypts = [];
 
 function initMap() {
-    document.getElementById("selectedList").style.display = "none";
+    // document.getElementById("selectedList").style.display = "none";
 
     directionsDisplay = new google.maps.DirectionsRenderer();
     directionsService = new google.maps.DirectionsService();
 
-    map = new google.maps.Map(document.getElementById('mapview'), {
+    map = new google.maps.Map(document.getElementById('View'), {
         center: { lat: 24.140793, lng: 120.676346 },
         zoom: 14,
         mapTypeControl: false,
@@ -30,150 +30,190 @@ function initMap() {
 
     infowindow = new google.maps.InfoWindow();
 
-    service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, callback);
+    // service = new google.maps.places.PlacesService(map);
+    // service.textSearch(request, callback);
 
-    makerArray = [];
-    selectArray = [];
-    waypts = [];
+    // makerArray = [];
+    // selectArray = [];
+    // waypts = [];
 
-    renderSelected();
+    // renderSelected();
     directionsDisplay.setMap(map);
 }
 
-function callback(results, status, pagination) { 
-    console.log(pagination);
-    if (status == google.maps.places.PlacesServiceStatus.OK) {  
-        data = JSON.stringify(results);
-        data = JSON.parse(data);
-        next_page = data;
-        console.log(data);
-        renderLine();
-    }else{
-        if (pagination.hasNextPage) {
-            var moreButton = document.getElementById('more');
-            moreButton.disabled = false;
-            moreButton.addEventListener('click', function() {
-                moreButton.disabled = true;
-                pagination.nextPage();
-            });
-        }
+// function callback(results, status, pagination) { 
+//     console.log(pagination);
+//     if (status == google.maps.places.PlacesServiceStatus.OK) {  
+//         data = JSON.stringify(results);
+//         data = JSON.parse(data);
+//         next_page = data;
+//         console.log(data);
+//         renderLine();
+//     }else{
+//         if (pagination.hasNextPage) {
+//             var moreButton = document.getElementById('more');
+//             moreButton.disabled = false;
+//             moreButton.addEventListener('click', function() {
+//                 moreButton.disabled = true;
+//                 pagination.nextPage();
+//             });
+//         }
  
+//     }
+// }
+
+
+// function pos(lat, lng, i) {
+//     var selected = { lat, lng, i };
+//     var isSelected = false;
+//     var len = selectArray.length;
+
+//     if (len == 0) {
+//         makerArray[makerArray.length] = new google.maps.Marker({   position: { lat: lat, lng: lng },   map: map,   title: 'Hello World!'  });
+//         map.setZoom(14);  
+//         map.setCenter({ lat: selected.lat, lng: selected.lng - 0.01 });
+//         selectArray.push(selected);
+//         renderSelected();
+//     } else {
+//         for (var i = 0; i < len; i++) {
+//             if (JSON.stringify(selectArray[i]) == JSON.stringify(selected)) {
+//                 isSelected = true;
+//                 break;
+//             }
+//         }
+//         if (isSelected != true) {
+//             makerArray[makerArray.length] = new google.maps.Marker({   position: { lat: lat, lng: lng },   map: map,   title: 'Hello World!'  });
+//             map.setZoom(14);  
+//             map.setCenter({ lat: selected.lat, lng: selected.lng - 0.01 });
+//             selectArray.push(selected);
+//             waypts.push({    
+//                 location: selected,
+//                 stopover: true   
+//             });
+//             renderSelected()
+//         } else {
+//             var str = '<div class="alert alert-warning alert-dismissible" role="alert">';
+//             str += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+//             str += '<strong>此地點已選取</strong></div>';
+//             document.getElementById('msg').innerHTML = str;
+//         }
+//     }
+//     showSteps();
+// }
+
+
+// function setMapOnAll(map) { 
+//     for (var i = 0; i < makerArray.length; i++) {  
+//         makerArray[i].setMap(map); 
+//     }
+// }
+
+// function calculateAndDisplayRoute() {
+//     setMapOnAll(null);
+//     var len = selectArray.length;
+//     var start = { lat: selectArray[0].lat, lng: selectArray[0].lng };
+//     var end = { lat: selectArray[len - 1].lat, lng: selectArray[len - 1].lng };
+
+//     var request = {
+//         origin: start,
+//         destination: start,
+//         travelMode: google.maps.TravelMode.WALKING,
+//         waypoints: waypts
+
+//     };
+//     directionsService.route(request, function(result, status) {
+//         if (status == google.maps.DirectionsStatus.OK) {
+//             directionsDisplay.setDirections(result);
+//         }
+//     });
+
+
+//     showSteps();
+// }
+
+// function showSteps() {
+//     for (var i = 0; i < makerArray.length; i++) {
+//         var title = data[selectArray[i].i].name;
+//         var address = data[selectArray[i].i].formatted_address;
+//         var place_id = data[selectArray[i].i].place_id;
+//     }
+// }
+
+
+
+// function attachInstructionText(marker, text) {
+//     google.maps.event.addListener(marker, 'click', function() {
+//         infowindow.setContent(text);
+//         infowindow.open(map, marker);
+//     });
+// }
+
+// function renderLine() {
+//     var str = "";
+//     var node = document.getElementById("line");
+
+//     for (var i = 0; i < data.length; i++) {
+//         var title = data[i].name;
+//         var adddress = data[i].formatted_address;
+//         var lat = data[i].geometry.location.lat;
+//         var lng = data[i].geometry.location.lng;
+//         str = str + '<button type="button" class="list-group-item" onclick="pos(' + lat + "," + lng + "," + i + ')">' + title + "<br>" + adddress + '</button>';
+//     }
+//     node.innerHTML = str;
+// }
+
+// function renderSelected() {
+//     var str = "";
+//     var node = document.getElementById("selected");
+
+//     for (var i = 0; i < selectArray.length; i++) {
+//         var chr = String.fromCharCode(65 + i);
+//         var title = data[selectArray[i].i].name;
+//         var adddress = data[selectArray[i].i].formatted_address;
+//         var str = str + '<a href="#" class="list-group-item"><span class="badge">' + chr + '</span>' + title + '<br>' + adddress + '</a>';
+//     }
+//     node.innerHTML = str;
+
+//     if (str.length > 0)
+//         document.getElementById("selectedList").style.display = "block";
+// }
+
+
+/* UI */
+
+function View(Anchor) {
+    // console.log(Anchor);
+    switch(Anchor){
+        case '#add':
+            add()
+            break
+        case '#route':
+            break
+        case '#map':
+            initMap()
+            break
+        default:
+            break            
+    }
+
+    function add(){
+        var View = document.getElementById('View')
+        View.innerHTML = "123123132132"
+    }
+
+}
+
+function Route(){
+    var fun_bar_li = document.getElementById('fun-bar').children
+    var Anchor;
+    for(var i = 0; i < fun_bar_li.length; i++){
+        fun_bar_li[i].addEventListener("click", function(){
+            Anchor = this.children[0].getAttribute('href');
+            console.log(Anchor)
+            View(Anchor)
+        })
+
     }
 }
 
-
-function pos(lat, lng, i) {
-    var selected = { lat, lng, i };
-    var isSelected = false;
-    var len = selectArray.length;
-
-    if (len == 0) {
-        makerArray[makerArray.length] = new google.maps.Marker({   position: { lat: lat, lng: lng },   map: map,   title: 'Hello World!'  });
-        map.setZoom(14);  
-        map.setCenter({ lat: selected.lat, lng: selected.lng - 0.01 });
-        selectArray.push(selected);
-        renderSelected();
-    } else {
-        for (var i = 0; i < len; i++) {
-            if (JSON.stringify(selectArray[i]) == JSON.stringify(selected)) {
-                isSelected = true;
-                break;
-            }
-        }
-        if (isSelected != true) {
-            makerArray[makerArray.length] = new google.maps.Marker({   position: { lat: lat, lng: lng },   map: map,   title: 'Hello World!'  });
-            map.setZoom(14);  
-            map.setCenter({ lat: selected.lat, lng: selected.lng - 0.01 });
-            selectArray.push(selected);
-            waypts.push({    
-                location: selected,
-                stopover: true   
-            });
-            renderSelected()
-        } else {
-            var str = '<div class="alert alert-warning alert-dismissible" role="alert">';
-            str += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-            str += '<strong>此地點已選取</strong></div>';
-            document.getElementById('msg').innerHTML = str;
-        }
-    }
-    showSteps();
-}
-
-
-function setMapOnAll(map) { 
-    for (var i = 0; i < makerArray.length; i++) {  
-        makerArray[i].setMap(map); 
-    }
-}
-
-function calculateAndDisplayRoute() {
-    setMapOnAll(null);
-    var len = selectArray.length;
-    var start = { lat: selectArray[0].lat, lng: selectArray[0].lng };
-    var end = { lat: selectArray[len - 1].lat, lng: selectArray[len - 1].lng };
-
-    var request = {
-        origin: start,
-        destination: start,
-        travelMode: google.maps.TravelMode.WALKING,
-        waypoints: waypts
-
-    };
-    directionsService.route(request, function(result, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(result);
-        }
-    });
-
-
-    showSteps();
-}
-
-function showSteps() {
-    for (var i = 0; i < makerArray.length; i++) {
-        var title = data[selectArray[i].i].name;
-        var address = data[selectArray[i].i].formatted_address;
-        var place_id = data[selectArray[i].i].place_id;
-    }
-}
-
-
-
-function attachInstructionText(marker, text) {
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(text);
-        infowindow.open(map, marker);
-    });
-}
-
-function renderLine() {
-    var str = "";
-    var node = document.getElementById("line");
-
-    for (var i = 0; i < data.length; i++) {
-        var title = data[i].name;
-        var adddress = data[i].formatted_address;
-        var lat = data[i].geometry.location.lat;
-        var lng = data[i].geometry.location.lng;
-        str = str + '<button type="button" class="list-group-item" onclick="pos(' + lat + "," + lng + "," + i + ')">' + title + "<br>" + adddress + '</button>';
-    }
-    node.innerHTML = str;
-}
-
-function renderSelected() {
-    var str = "";
-    var node = document.getElementById("selected");
-
-    for (var i = 0; i < selectArray.length; i++) {
-        var chr = String.fromCharCode(65 + i);
-        var title = data[selectArray[i].i].name;
-        var adddress = data[selectArray[i].i].formatted_address;
-        var str = str + '<a href="#" class="list-group-item"><span class="badge">' + chr + '</span>' + title + '<br>' + adddress + '</a>';
-    }
-    node.innerHTML = str;
-
-    if (str.length > 0)
-        document.getElementById("selectedList").style.display = "block";
-}
+Route()
